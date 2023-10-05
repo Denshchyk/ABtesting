8357846f3ecdf20251f2108b1839d17f01065b64
pavlodenshchyk <bloody.squirr3l@gmail.com>
2023-10-05 15:36:45 +0300

    add experiment service

--- ABtesting.Service/ExperimentService.cs
+++ ABtesting.Service/ExperimentService.cs
@@ -1,6 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace ABtesting.Service;
 
 public class ExperimentService
 {
-    
+    private ApplicationContext _context;
+
+    public ExperimentService(ApplicationContext context)
+    {
+        _context = context;
+    }
+    public async Task AddExperimentAsync(Experiment addExperiment)
+    {
+        await _context.Experiments.AddAsync(addExperiment);
+        await _context.SaveChangesAsync();
+    }
+    public async Task<Experiment?> GetByKeyAsync(string key)
+    {
+        var experiment = await _context.Experiments.FirstOrDefaultAsync(experiment => experiment.Key == key);
+        return experiment;
+    }
 }
